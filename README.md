f8716cbdab297b82a56a9bed2e3e6440262c653f
bhyunki <bhyunki93@gmail.com>
2021-07-13 20:13:38 +0900

    Added #3 937_Reorder_Data_in_Log_Files

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 |---| ----- | -------- | ---------- |
 |1|[125. Valid Palindrome](https://leetcode.com/problems/valid-palindrome/) | [Python3](./srcs/python3/125_valid_palindrome.py) |Easy|
 |2|[344. Reverse String](https://leetcode.com/problems/reverse-string/) | [Python3](./srcs/python3/344_reverse_string.py) |Easy|
+|3|[937. Reorder Data in Log Files](https://leetcode.com/problems/reorder-data-in-log-files/) | [Python3](./srcs/python3/937_reorder_data_in_log_files.py) |Easy|
---
+++ srcs/python3/937_reorder_data_in_log_files.py
@@ -0,0 +1,23 @@
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        llog=[]
+        dlog=[]
+        for log in logs:
+            k = log.split()[0]
+            v = log.split()[1:]
+
+            if v[0].isdigit():
+                # digit log
+                dlog.append([k, v])
+            else:
+                # letter log
+                llog.append([k, v])
+            
+        sort = sorted(llog, key=lambda x:(x[1],x[0]))
+        for t in dlog:
+            sort.append(t)
+        
+        answer=[]
+        for log in sort:
+            answer.append(log[0]+' '+' '.join(str(e) for e in log[1]))
+        return answer
